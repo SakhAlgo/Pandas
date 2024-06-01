73cec755421a4e3128efa6b3830a457b971e86a9
SakhAlgo <sakhalgo@gmail.com>
2024-06-01 20:29:02 +1100

    task1

--- Task1/main.py
+++ Task1/main.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-dt = pd.read_csv('/data/googleplaystore.csv')
-dt.head()
+df = pd.read_csv('data/GoogleApps.csv')
+# print(df.info())
+# avg_price = dt[dt['Price'].mean()]
+# print('Средняя цена приложения:', avg_price)
+print(df[df['Rating'] > 4.9]['Installs'].median())
